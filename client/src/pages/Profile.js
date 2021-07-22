79168f8eb217f7bd7c2f2f0b5c91659c5032b839
jayeebee <pnuthexsa311@gmail.com>
2021-07-22 14:56:56 -0400

    adapted profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,8 @@
-import { Container, Button } from 'react-bootstrap'
+import { Container, Jumbotron, Card, CardColumns, Button } from 'react-bootstrap'
 import { DELETE_USER } from '../utils/mutations'
-import { useMutation } from '@apollo/react-hooks'
 import Auth from '../utils/auth'
+import { GET_ME } from '../utils/queries'
+import { useMutation, useQuery } from '@apollo/react-hooks'
 //import {removeUser} from 
 
 const Profile = () => {
@@ -25,16 +26,61 @@ const Profile = () => {
             console.error(err)
         }
     } */
+    const { loading, data} = useQuery(GET_ME)
 
+    const userData = data?.me || {};
+
+    const songList = [
+        {
+            title: "Blackanese",
+            artist: "Kazu",
+            src: "../assets/Blackanese.mp3"
+        },
+        {
+            title: "That's My DJ",
+            artist: "Girl Talk",
+            src: "../assets/ThatsMyDJ"
+        }
+    ];
+
+    const song = songList[0];
+    
+    if (loading) {
+        return <h2>LOADING...</h2>;
+      }
 
 
     return (
-        <Container>
-        <h1>Your Profile</h1>
-        <Button onClick={() => console.log("test")}/*className='btn-block btn-danger' onClick={() => handleDeleteUser(user.userId)}*/ >
-         Delete Account 
-         </Button>
-        </Container>
+        <>
+        <Jumbotron fluid className='text-light bg-dark'>
+          <Container>
+            <h1>Your Profile</h1>
+          </Container>
+          </Jumbotron>
+          <Container>
+          <h2>
+            {songList.length
+              ? `Viewing ${songList.length} saved ${songList.length === 1 ? 'song' : 'songs'}`
+              : 'You have no active songs!'} </h2>
+              <Container style={{width: "100%", display: 'flex', flexDirection: 'row' }}>
+                  
+                  <Card border="primary" style={{width: "25%"}}>
+                  <center><Card.Title>{songList[0].title}</Card.Title></center>
+                  <center><Card.Subtitle>by</Card.Subtitle></center>
+                  <center><Card.Text>{songList[0].artist}</Card.Text></center>
+                  </Card>
+  
+                  <Card style={{width: "25%"}}>
+                  <center><Card.Title>{songList[1].title}</Card.Title></center>
+                  <center><Card.Subtitle className="text-muted">by</Card.Subtitle></center>
+                  <center><Card.Text>{songList[1].artist}</Card.Text></center>
+                  </Card>
+              </Container>
+          <Button onClick={() => console.log("test")}/*className='btn-block btn-danger' onClick={() => handleDeleteUser(user.userId)}*/ >
+           Delete Account 
+           </Button>
+              </Container>
+      </>
     )
 }
     
